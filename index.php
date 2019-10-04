ac7456454c6a0d61037423aca7e0161d05bbe9ff
Tuukka Puoliväli <puolivali@gmail.com>
2019-10-04 14:13:52 +0300

    Minor style change

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php require_once('header.html'); ?>
 <div id="center">
+    <h1>URL Shortener</h1>
+    <div id="center-form">
     <form method="POST" ACTION="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/Addurl.php'; ?>">
-        <input name="url" type="ttext">
-        <input type="submit" name="submit">
+        <input name="url" type="text" id="center-form-input" value="http:&#47;&#47;..." onfocus="if(this.value  == 'http:&#47;&#47;...') { this.value = ''; } " onblur="if(this.value == '') { this.value = 'http:&#47;&#47;...'; } ">
+        <input type="submit" name="submit" value="Shorten" id="center-form-submit">
     </form>
+    </div>
 </div>
 <?php require_once('footer.html'); ?>
