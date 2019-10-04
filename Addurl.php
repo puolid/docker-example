ffdc2f358dac12ccff2771e8e5933fa80b7cb5d6
Tuukka Puoliväli <puolivali@gmail.com>
2019-10-04 14:12:36 +0300

    Create short url using adler32 hash instead of uniqid

--- Addurl.php
+++ Addurl.php
@@ -9,12 +9,20 @@ if (isset($_POST["url"]))
     // Create db connection
     $db = new Database();
 
-    $shorturl = uniqid();
-
+    $uniqid = uniqid();
     $url = htmlspecialchars($_POST['url']);
+
+    $ishttpset = substr($url, 0, 7);
+    if ($ishttpset != 'http://')
+    {
+        $url = 'http://' . $url;
+    }
+
+    $shorturl = hash("adler32", $url . $uniqid);
+
     $db->addUrl($url, $shorturl);
 }
-echo '<div id="center"><p>http://' . $_SERVER['HTTP_HOST'] . '/Geturl.php?url=' . $shorturl . '</p></div>';
+echo '<div id="center"><input type="text" onClick="this.setSelectionRange(0, this.value.length)" value="' . $_SERVER['HTTP_HOST'] . '/Geturl.php?url=' . $shorturl . '"></div>';
 
 require_once('footer.html');
 ?>
